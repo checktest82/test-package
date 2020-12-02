56ef11fb256106838950e8a03ed8d82b0040f1dc
checktest82 <75334175+checktest82@users.noreply.github.com>
2020-12-01 23:11:20 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -10,6 +11,7 @@ import os
 app = Flask(__name__)
 PORT = os.environ.get('PORT') or 5057
 
+firefox_binary = FirefoxBinary('/app/vendor/firefox/firefox')
 FF_options = webdriver.FirefoxOptions()
 FF_profile = webdriver.FirefoxProfile()
 FF_options.add_argument("-headless")
@@ -23,7 +25,7 @@ FF_profile.update_preferences()
 @app.route("/get")
 def getcurrIp():
     try:
-        driver = webdriver.Firefox(options=FF_options, firefox_profile=FF_profile, executable_path='/app/vendor/geckodriver/geckodriver')
+        driver = webdriver.Firefox(options=FF_options, firefox_profile=FF_profile, executable_path='/app/vendor/geckodriver/geckodriver', firefox_binary=firefox_binary)
         driver.get("https://icanhazip.com")
         ipaddr = driver.page_source
         driver.close()
